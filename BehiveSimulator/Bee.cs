841b2592333db5c4d29e5371375c1c88e9bdce06
Carlos <carlosdiones28@gmail.com>
2022-10-20 20:41:26 -0300

    Adicionar arquivos de projeto.

---
+++ BehiveSimulator/Bee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BehiveSimulator
+{
+    class Bee
+    {
+        private const double HoneyConsumed = 0.5;
+        private const int MoveRate = 3;
+   
+    }
+}

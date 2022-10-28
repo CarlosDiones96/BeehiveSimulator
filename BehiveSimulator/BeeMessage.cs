b23fcb12323d3d6d2d1df78177b769666ad15005
Carlos <carlosdiones28@gmail.com>
2022-10-28 18:12:28 -0300

    creates BeeMessage delegate

---
+++ BehiveSimulator/BeeMessage.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BehiveSimulator
+{
+    public delegate void BeeMessage(int ID, string Message);
+}

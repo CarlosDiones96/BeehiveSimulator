53c375181a1e627fab74a7d6efe91db3d710cb7b
Carlos <carlosdiones28@gmail.com>
2022-11-01 15:48:22 -0300

    updates and adds files

---
+++ BehiveSimulator/Renderer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BehiveSimulator
+{
+    class Renderer
+    {
+        private World world;
+        private HiveForm hiveForm;
+        private FieldForm fieldForm;
+
+        private Dictionary<Flower, PictureBox> flowerLookup = new Dictionary<Flower, PictureBox>();
+        private Dictionary<Bee, BeeControl> beeLookup = new Dictionary<Bee, BeeControl>();
+    }
+}

790f2bf548cfa41a181ce9971f0d09dd06c43a05
Carlos Diones <carlosdiones28@gmail.com>
2022-11-24 16:03:14 -0300

    creates a zoomer to experiment with graphics

--- BeeControl on a form/Form1.Designer.cs
+++ BeeControl on a form/Form1.Designer.cs
@@ -28,34 +28,62 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // trackBar1
             // 
-            this.button1.Location = new System.Drawing.Point(174, 204);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Add";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.trackBar1.BackColor = System.Drawing.Color.White;
+            this.trackBar1.Location = new System.Drawing.Point(12, 355);
+            this.trackBar1.Maximum = 300;
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(414, 45);
+            this.trackBar1.TabIndex = 0;
+            this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.trackBar1.Value = 175;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.BackColor = System.Drawing.Color.White;
+            this.trackBar2.Location = new System.Drawing.Point(430, 12);
+            this.trackBar2.Maximum = 300;
+            this.trackBar2.Minimum = 1;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar2.Size = new System.Drawing.Size(45, 306);
+            this.trackBar2.TabIndex = 1;
+            this.trackBar2.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.trackBar2.Value = 175;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(487, 337);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(487, 423);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Zoomer";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
     }
 }
 
--- BeeControl on a form/Form1.cs
+++ BeeControl on a form/Form1.cs
@@ -12,28 +12,39 @@ namespace BeeControl_on_a_form
 {
     public partial class Form1 : Form
     {
+        Bitmap photo = new Bitmap(@"C:\Users\dione\Pictures\csharp.png");
         public Form1()
         {
             InitializeComponent();
         }
 
-        BeeControl control = null;
-
         private void button1_Click(object sender, EventArgs e)
         {
-            if (control == null)
-            {
-                control = new BeeControl() { Location = new Point(100, 100) };
-                Controls.Add(control);
-            }
-            else
-            {
-                using (control)
-                {
-                    Controls.Remove(control);
-                }
-                control = null;
-            }
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            Invalidate();
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            Invalidate();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.FillRectangle(Brushes.White, 0, 0, Width, Height);
+            g.DrawImage(photo, 10, 10, trackBar1.Value, trackBar2.Value);
+        }
+
+        //test code [to be removed]
+        protected override void OnPaint(PaintEventArgs e)
+        {
+          
+            Console.WriteLine("OnPaint "+  DateTime.Now +  e.ClipRectangle.ToString());
+            base.OnPaint(e);
         }
     }
 }
